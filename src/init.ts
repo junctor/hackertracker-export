631c8e45d162ee4a6be159cd925255aa58d362f7
cak <cak@fastmail.com>
2024-03-18 05:35:03 -0400

    fix trailing space on filename

--- src/init.ts
+++ src/init.ts
@@ -1,7 +1,7 @@
 import { type Firestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore/lite";
-import { firebaseConfig } from "./ config";
+import { firebaseConfig } from "./config";
 
 export default async function firebaseInit(apiKey: string): Promise<Firestore> {
   const config = firebaseConfig(apiKey);
