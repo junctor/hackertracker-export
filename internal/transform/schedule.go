e7583f80237c903c764368dc01969866170ca978
cak <cak@typeerror.com>
2026-06-20 21:26:36 -0400

    Refactor schedule handling to replace event terminology with session terminology; enhance data structures and improve session-related functions for better clarity and functionality

--- internal/transform/schedule.go
+++ internal/transform/schedule.go
@@ -8,123 +8,176 @@ import (
 )
 
 type builtIndexes struct {
-	eventsByDay map[string][]int
-	eventsByTag map[string][]int
+	sessionsByDay map[string][]int
+	sessionsByTag map[string][]int
+}
+
+type SessionViewModel struct {
+	Begin                 string        `json:"begin"`
+	BeginDisplay          string        `json:"beginDisplay"`
+	BeginIso              string        `json:"beginIso"`
+	BeginTimestampSeconds int64         `json:"beginTimestampSeconds"`
+	Color                 string        `json:"color"`
+	ContentEntity         *ContentModel `json:"contentEntity"`
+	ContentID             int           `json:"contentId"`
+	End                   string        `json:"end"`
+	EndDisplay            string        `json:"endDisplay"`
+	EndIso                string        `json:"endIso"`
+	EndTimestampSeconds   int64         `json:"endTimestampSeconds"`
+	ID                    int           `json:"id"`
+	LocationName          string        `json:"locationName"`
+	Session               SessionModel  `json:"session"`
+	Speakers              *string       `json:"speakers"`
+	Tags                  []CompactTag  `json:"tags"`
+	Title                 string        `json:"title"`
+}
+
+type ScheduleDay struct {
+	Day      string             `json:"day"`
+	Sessions []SessionViewModel `json:"sessions"`
+}
+
+type LocationCard struct {
+	ID        int     `json:"id"`
+	Name      string  `json:"name"`
+	ParentID  *int    `json:"parentId"`
+	ShortName *string `json:"shortName"`
+}
+
+type TagDetail struct {
+	Days []ScheduleDay `json:"days"`
+	Tag  TagModel      `json:"tag"`
+}
+
+type LocationDetail struct {
+	Days     []ScheduleDay `json:"days"`
+	Location LocationModel `json:"location"`
+}
+
+type SessionDetail struct {
+	Content  ContentModel   `json:"content"`
+	Location *LocationModel `json:"location"`
+	People   []PersonModel  `json:"people"`
+	Session  SessionModel   `json:"session"`
+	Tags     []TagModel     `json:"tags"`
 }
 
 func buildIndexes(st *stores, timezone string) builtIndexes {
-	eventsByDay := map[string][]int{}
-	eventsByTag := map[string][]int{}
-	eventStarts := map[int]int64{}
+	sessionsByDay := map[string][]int{}
+	sessionsByTag := map[string][]int{}
+	sessionStarts := map[int]int64{}
 
-	for _, eventID := range st.eventIDs {
-		event := st.eventsByID[eventID]
-		eventStarts[eventID] = timestampSeconds(stringValue(event["begin"]))
-		if day := eventDay(stringValue(event["begin"]), timezone); day != "" {
-			eventsByDay[day] = append(eventsByDay[day], eventID)
+	for _, sessionID := range st.sessionIDs {
+		session := st.sessionsByID[sessionID]
+		sessionStarts[sessionID] = session.BeginTimestampSeconds
+		if day := sessionDay(session.Begin, timezone); day != "" {
+			sessionsByDay[day] = append(sessionsByDay[day], sessionID)
 		}
-		for _, tagID := range intSlice(event["tagIds"]) {
-			eventsByTag[stringValue(tagID)] = append(eventsByTag[stringValue(tagID)], eventID)
+		for _, tagID := range session.TagIDs {
+			sessionsByTag[idKey(tagID)] = append(sessionsByTag[idKey(tagID)], sessionID)
 		}
 	}
-	sortEventIndex(eventsByDay, eventStarts)
-	sortEventIndex(eventsByTag, eventStarts)
-	return builtIndexes{eventsByDay: eventsByDay, eventsByTag: eventsByTag}
+	sortSessionIndex(sessionsByDay, sessionStarts)
+	sortSessionIndex(sessionsByTag, sessionStarts)
+	return builtIndexes{sessionsByDay: sessionsByDay, sessionsByTag: sessionsByTag}
 }
 
-func buildScheduleEventViewModel(event map[string]any, st *stores, timezone string) map[string]any {
-	speakerIDs := intSlice(event["speakerIds"])
-	if len(speakerIDs) == 0 {
-		speakerIDs = intSlice(event["personIds"])
-	}
+func buildScheduleSessionViewModel(session SessionModel, st *stores) SessionViewModel {
 	speakerNames := []string{}
-	for _, personID := range speakerIDs {
-		if person := st.peopleByID[personID]; person != nil && person["name"] != nil {
-			speakerNames = append(speakerNames, stringValue(person["name"]))
+	for _, personID := range session.PersonIDs {
+		if person, ok := st.peopleByID[personID]; ok && person.Name != "" {
+			speakerNames = append(speakerNames, person.Name)
 		}
 	}
-	contentID, _ := normalizeID(event["contentId"])
-	var contentEntity any
-	if contentID != 0 {
-		contentEntity = st.contentByID[contentID]
+
+	var contentEntity *ContentModel
+	if content, ok := st.contentByID[session.ContentID]; ok {
+		contentEntity = &content
 	}
+
 	locationName := "Unknown location"
-	if locationID, ok := normalizeID(event["locationId"]); ok {
-		if loc := st.locationsByID[locationID]; loc != nil && loc["name"] != nil {
-			locationName = stringValue(loc["name"])
+	if session.LocationID != nil {
+		if location, ok := st.locationsByID[*session.LocationID]; ok && location.Name != "" {
+			locationName = location.Name
 		}
 	}
-	tags := []any{}
-	for _, tagID := range intSlice(event["tagIds"]) {
-		if tag := st.tagsByID[tagID]; tag != nil {
+
+	tags := []CompactTag{}
+	for _, tagID := range session.TagIDs {
+		if tag, ok := st.tagsByID[tagID]; ok {
 			tags = append(tags, compactTag(tag))
 		}
 	}
-	var speakers any
+
+	var speakers *string
 	if len(speakerNames) > 0 {
-		speakers = strings.Join(speakerNames, ", ")
+		text := strings.Join(speakerNames, ", ")
+		speakers = &text
 	}
-	begin := stringValue(event["begin"])
-	end := stringValue(event["end"])
-	return map[string]any{
-		"begin":                 begin,
-		"beginDisplay":          nonEmptyString(event["beginDisplay"], eventTimeTable(begin, true, timezone)),
-		"beginIso":              nonEmptyString(event["beginIso"], isoTime(begin)),
-		"beginTimestampSeconds": timestampSeconds(begin),
-		"color":                 nonEmptyString(event["color"], ""),
-		"contentEntity":         contentEntity,
-		"contentId":             event["contentId"],
-		"end":                   end,
-		"endDisplay":            nonEmptyString(event["endDisplay"], eventTimeTable(end, false, timezone)),
-		"endIso":                nonEmptyString(event["endIso"], isoTime(end)),
-		"endTimestampSeconds":   timestampSeconds(end),
-		"id":                    event["id"],
-		"locationName":          locationName,
-		"session":               event,
-		"speakers":              speakers,
-		"tags":                  tags,
-		"title":                 event["title"],
+
+	return SessionViewModel{
+		Begin:                 session.Begin,
+		BeginDisplay:          firstNonEmpty(session.BeginDisplay, sessionTimeTable(session.Begin, true, "")),
+		BeginIso:              firstNonEmpty(session.BeginIso, isoTime(session.Begin)),
+		BeginTimestampSeconds: session.BeginTimestampSeconds,
+		Color:                 "",
+		ContentEntity:         contentEntity,
+		ContentID:             session.ContentID,
+		End:                   session.End,
+		EndDisplay:            firstNonEmpty(session.EndDisplay, sessionTimeTable(session.End, false, "")),
+		EndIso:                firstNonEmpty(session.EndIso, isoTime(session.End)),
+		EndTimestampSeconds:   session.EndTimestampSeconds,
+		ID:                    session.ID,
+		LocationName:          locationName,
+		Session:               session,
+		Speakers:              speakers,
+		Tags:                  tags,
+		Title:                 session.Title,
 	}
 }
 
 func buildPageReadyArtifacts(st *stores, indexes builtIndexes, timezone string) (map[string]any, map[string]map[int]any) {
-	allEvents := make([]map[string]any, 0, len(st.eventIDs))
-	modelsByEventID := map[int]map[string]any{}
-	for _, eventID := range st.eventIDs {
-		event := st.eventsByID[eventID]
-		allEvents = append(allEvents, event)
-		modelsByEventID[eventID] = buildScheduleEventViewModel(event, st, timezone)
+	allSessions := make([]SessionModel, 0, len(st.sessionIDs))
+	modelsBySessionID := map[int]SessionViewModel{}
+	for _, sessionID := range st.sessionIDs {
+		session := st.sessionsByID[sessionID]
+		allSessions = append(allSessions, session)
+		modelsBySessionID[sessionID] = buildScheduleSessionViewModel(session, st)
 	}
-	scheduleDays := buildAllScheduleDays(st, indexes, modelsByEventID, timezone)
-	bookmarkEventsByID := map[string]any{}
-	for _, eventID := range st.eventIDs {
-		if model := modelsByEventID[eventID]; model != nil {
-			bookmarkEventsByID[stringValue(eventID)] = model
+
+	scheduleDays := buildAllScheduleDays(st, indexes, modelsBySessionID, timezone)
+	bookmarkSessionsByID := map[string]SessionViewModel{}
+	for _, sessionID := range st.sessionIDs {
+		if model, ok := modelsBySessionID[sessionID]; ok {
+			bookmarkSessionsByID[idKey(sessionID)] = model
 		}
 	}
-	locationCards := []any{}
+
+	locationCards := []LocationCard{}
 	for _, locationID := range st.locationIDs {
 		location := st.locationsByID[locationID]
-		locationCards = append(locationCards, map[string]any{
-			"id":        location["id"],
-			"name":      location["name"],
-			"parentId":  location["parentId"],
-			"shortName": location["shortName"],
+		locationCards = append(locationCards, LocationCard{
+			ID:        location.ID,
+			Name:      location.Name,
+			ParentID:  location.ParentID,
+			ShortName: location.ShortName,
 		})
 	}
-	announcements := []map[string]any{}
+
+	announcements := []ArticleModel{}
 	for _, articleID := range st.articleIDs {
 		announcements = append(announcements, st.articlesByID[articleID])
 	}
-	slices.SortFunc(announcements, func(a, b map[string]any) int {
+	slices.SortFunc(announcements, func(a, b ArticleModel) int {
 		return cmp.Or(
-			cmp.Compare(int64Value(b["updatedAtMs"]), int64Value(a["updatedAtMs"])),
-			cmp.Compare(intValue(a["id"]), intValue(b["id"])),
+			cmp.Compare(valueOrZero(b.UpdatedAtMs), valueOrZero(a.UpdatedAtMs)),
+			cmp.Compare(a.ID, b.ID),
 		)
 	})
 
 	details := map[string]map[int]any{
 		"content":       {},
+		"sessions":      {},
 		"people":        {},
 		"tags":          {},
 		"locations":     {},
@@ -132,29 +185,31 @@ func buildPageReadyArtifacts(st *stores, indexes builtIndexes, timezone string) 
 		"organizations": {},
 	}
 	for _, contentID := range st.contentIDs {
-		details["content"][contentID] = buildContentDetail(st.contentByID[contentID], st, allEvents)
+		details["content"][contentID] = buildContentDetail(st.contentByID[contentID], st, allSessions)
+	}
+	for _, sessionID := range st.sessionIDs {
+		details["sessions"][sessionID] = buildSessionDetail(st.sessionsByID[sessionID], st)
 	}
 	for _, personID := range st.peopleIDs {
-		details["people"][personID] = buildPersonDetail(st.peopleByID[personID], st, allEvents)
+		details["people"][personID] = buildPersonDetail(st.peopleByID[personID], st, allSessions)
 	}
 	for _, tagID := range st.tagIDs {
-		tag := st.tagsByID[tagID]
-		details["tags"][tagID] = map[string]any{
-			"days": buildScheduleDaysFromEvents(eventsFromIDs(eventIDsForTag(tagID, st, indexes), st.eventsByID), modelsByEventID, timezone),
-			"tag":  tag,
+		details["tags"][tagID] = TagDetail{
+			Days: buildScheduleDaysFromSessions(sessionsFromIDs(sessionIDsForTag(tagID, st, indexes), st.sessionsByID), modelsBySessionID, timezone),
+			Tag:  st.tagsByID[tagID],
 		}
 	}
 	for _, locationID := range st.locationIDs {
 		location := st.locationsByID[locationID]
-		events := []map[string]any{}
-		for _, event := range allEvents {
-			if intValue(event["locationId"]) == locationID {
-				events = append(events, event)
+		sessions := []SessionModel{}
+		for _, session := range allSessions {
+			if session.LocationID != nil && *session.LocationID == locationID {
+				sessions = append(sessions, session)
 			}
 		}
-		details["locations"][locationID] = map[string]any{
-			"days":     buildScheduleDaysFromEvents(events, modelsByEventID, timezone),
-			"location": location,
+		details["locations"][locationID] = LocationDetail{
+			Days:     buildScheduleDaysFromSessions(sessions, modelsBySessionID, timezone),
+			Location: location,
 		}
 	}
 	for _, id := range st.documentIDs {
@@ -163,92 +218,117 @@ func buildPageReadyArtifacts(st *stores, indexes builtIndexes, timezone string) 
 	for _, id := range st.organizationIDs {
 		details["organizations"][id] = st.organizationsByID[id]
 	}
+
 	return map[string]any{
-		"announcementsList":  announcements,
-		"bookmarkEventsById": bookmarkEventsByID,
-		"locationCards":      locationCards,
-		"scheduleDays":       scheduleDays,
+		"announcementsList":    announcements,
+		"bookmarkSessionsById": bookmarkSessionsByID,
+		"locationCards":        locationCards,
+		"scheduleDays":         scheduleDays,
 	}, details
 }
 
-func buildAllScheduleDays(st *stores, indexes builtIndexes, modelsByEventID map[int]map[string]any, timezone string) []any {
-	keys := slices.Sorted(maps.Keys(indexes.eventsByDay))
-	days := []any{}
+func buildAllScheduleDays(st *stores, indexes builtIndexes, modelsBySessionID map[int]SessionViewModel, timezone string) []ScheduleDay {
+	keys := slices.Sorted(maps.Keys(indexes.sessionsByDay))
+	days := []ScheduleDay{}
 	for _, day := range keys {
-		events := eventsFromIDs(indexes.eventsByDay[day], st.eventsByID)
-		models := modelsForEvents(events, modelsByEventID)
+		sessions := sessionsFromIDs(indexes.sessionsByDay[day], st.sessionsByID)
+		models := modelsForSessions(sessions, modelsBySessionID)
 		if len(models) > 0 {
-			days = append(days, map[string]any{"day": day, "events": models})
+			days = append(days, ScheduleDay{Day: day, Sessions: models})
 		}
 	}
 	if len(keys) > 0 {
 		return days
 	}
-	return buildScheduleDaysFromEvents(eventsFromIDs(st.eventIDs, st.eventsByID), modelsByEventID, timezone)
+	return buildScheduleDaysFromSessions(sessionsFromIDs(st.sessionIDs, st.sessionsByID), modelsBySessionID, timezone)
 }
 
-func buildScheduleDaysFromEvents(events []map[string]any, modelsByEventID map[int]map[string]any, timezone string) []any {
-	groups := map[string][]map[string]any{}
-	for _, event := range events {
-		day := eventDay(stringValue(event["begin"]), timezone)
+func buildScheduleDaysFromSessions(sessions []SessionModel, modelsBySessionID map[int]SessionViewModel, timezone string) []ScheduleDay {
+	groups := map[string][]SessionModel{}
+	for _, session := range sessions {
+		day := sessionDay(session.Begin, timezone)
 		if day == "" {
 			continue
 		}
-		groups[day] = append(groups[day], event)
+		groups[day] = append(groups[day], session)
 	}
 	keys := slices.Sorted(maps.Keys(groups))
-	out := []any{}
+	out := []ScheduleDay{}
 	for _, day := range keys {
-		models := modelsForEvents(groups[day], modelsByEventID)
+		models := modelsForSessions(groups[day], modelsBySessionID)
 		if len(models) > 0 {
-			out = append(out, map[string]any{"day": day, "events": models})
+			out = append(out, ScheduleDay{Day: day, Sessions: models})
 		}
 	}
 	return out
 }
 
-func modelsForEvents(events []map[string]any, modelsByEventID map[int]map[string]any) []any {
-	sortEvents(events)
-	models := []any{}
-	for _, event := range events {
-		if model := modelsByEventID[intValue(event["id"])]; model != nil {
+func modelsForSessions(sessions []SessionModel, modelsBySessionID map[int]SessionViewModel) []SessionViewModel {
+	sortSessions(sessions)
+	models := []SessionViewModel{}
+	for _, session := range sessions {
+		if model, ok := modelsBySessionID[session.ID]; ok {
 			models = append(models, model)
 		}
 	}
 	return models
 }
 
-func sortEvents(events []map[string]any) {
-	slices.SortFunc(events, func(a, b map[string]any) int {
+func sortSessions(sessions []SessionModel) {
+	slices.SortFunc(sessions, func(a, b SessionModel) int {
 		return cmp.Or(
-			cmp.Compare(timestampSeconds(stringValue(a["begin"])), timestampSeconds(stringValue(b["begin"]))),
-			cmp.Compare(intValue(a["id"]), intValue(b["id"])),
+			cmp.Compare(a.BeginTimestampSeconds, b.BeginTimestampSeconds),
+			cmp.Compare(a.ID, b.ID),
 		)
 	})
 }
 
-func eventsFromIDs(ids []int, byID map[int]map[string]any) []map[string]any {
-	events := []map[string]any{}
+func sessionsFromIDs(ids []int, byID map[int]SessionModel) []SessionModel {
+	sessions := []SessionModel{}
 	for _, id := range ids {
-		if event := byID[id]; event != nil {
-			events = append(events, event)
+		if session, ok := byID[id]; ok {
+			sessions = append(sessions, session)
 		}
 	}
-	return events
+	return sessions
 }
 
-func eventIDsForTag(tagID int, st *stores, indexes builtIndexes) []int {
-	if indexed, ok := indexes.eventsByTag[stringValue(tagID)]; ok {
+func sessionIDsForTag(tagID int, st *stores, indexes builtIndexes) []int {
+	if indexed, ok := indexes.sessionsByTag[idKey(tagID)]; ok {
 		return indexed
 	}
 	out := []int{}
-	for _, eventID := range st.eventIDs {
-		for _, id := range intSlice(st.eventsByID[eventID]["tagIds"]) {
+	for _, sessionID := range st.sessionIDs {
+		for _, id := range st.sessionsByID[sessionID].TagIDs {
 			if id == tagID {
-				out = append(out, eventID)
+				out = append(out, sessionID)
 				break
 			}
 		}
 	}
 	return out
+}
+
+func buildSessionDetail(session SessionModel, st *stores) SessionDetail {
+	content := st.contentByID[session.ContentID]
+	var location *LocationModel
+	if session.LocationID != nil {
+		if model, ok := st.locationsByID[*session.LocationID]; ok {
+			location = &model
+		}
+	}
+	return SessionDetail{
+		Content:  content,
+		Location: location,
+		People:   peopleForIDs(session.PersonIDs, st),
+		Session:  session,
+		Tags:     tagsForIDs(session.TagIDs, st.tagsByID),
+	}
+}
+
+func valueOrZero(value *int64) int64 {
+	if value == nil {
+		return 0
+	}
+	return *value
 }
